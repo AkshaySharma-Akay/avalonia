f05839319bb4610c6b9939be08e56373f1995da0
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2019-05-08 09:34:21 +0530

    adding routes to site and dashboard

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ResumeComponent } from './resume/resume.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './site/resume/resume.component';
+import { ProjectsComponent } from './site/projects/projects.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HomeComponent } from './home/home.component';
-import { ContactmeComponent } from './contactme/contactme.component';
+import { HomeComponent } from './site/home/home.component';
+import { ContactmeComponent } from './site/contactme/contactme.component';
+import { SitelayoutComponent } from './site/sitelayout/sitelayout.component';
+import { DashboardlayoutComponent } from './dashboard/dashboardlayout/dashboardlayout.component';
+import { DashboardhomeComponent } from './dashboard/dashboardhome/dashboardhome.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home',pathMatch:'full'},
-  { path: 'home', component:HomeComponent},
-  { path: 'resume',  component: ResumeComponent },
-  { path: 'projects', component: ProjectsComponent},
-  { path: 'contact', component: ContactmeComponent},
-  { path: '**', component: PageNotFoundComponent},
+  //site routes
+  {
+    path: '', component: SitelayoutComponent,
+    children: [
+      {path: '', redirectTo: '/home', pathMatch: 'full'},
+      { path: 'home', component: HomeComponent},
+      { path: 'resume', component: ResumeComponent },
+      { path: 'projects', component: ProjectsComponent },
+      { path: 'contact', component: ContactmeComponent },
+    ]
+  },
+  //dashboard routes
+  {
+    path: 'dashboard', component: DashboardlayoutComponent,
+    children: [
+      {path: '', component:DashboardhomeComponent,pathMatch: 'full'},
+    ]
+  },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ResumeComponent } from './resume/resume.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './site/resume/resume.component';
+import { ProjectsComponent } from './site/projects/projects.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HomeComponent } from './home/home.component';
-import { ContactmeComponent } from './contactme/contactme.component';
+import { HomeComponent } from './site/home/home.component';
+import { ContactmeComponent } from './site/contactme/contactme.component';
+import { SitelayoutComponent } from './site/sitelayout/sitelayout.component';
+import { DashboardlayoutComponent } from './dashboard/dashboardlayout/dashboardlayout.component';
+import { DashboardhomeComponent } from './dashboard/dashboardhome/dashboardhome.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { ContactmeComponent } from './contactme/contactme.component';
     ProjectsComponent,
     PageNotFoundComponent,
     HomeComponent,
-    ContactmeComponent
+    ContactmeComponent,
+    SitelayoutComponent,
+    DashboardlayoutComponent,
+    DashboardhomeComponent
   ],
   imports: [
     BrowserModule,
